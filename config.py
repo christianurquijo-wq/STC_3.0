071d1521f5bf4d81dd480f3c4022c5666809af2f
christianurquijo-wq <christian.urquijo@kuepa.com>
2026-08-25 18:50:56 +0000

    Mover IDs de hojas a variables de entorno (secretos), fuera del código

--- config.py
+++ config.py
@@ -1,54 +1,62 @@
 # config.py
+import os
 import time
+import streamlit as st
+
+def _obtener_config(nombre_env: str) -> str:
+    try:
+        return st.secrets[nombre_env]
+    except Exception:
+        return os.getenv(nombre_env)
 
 FUENTES = {
     "general": {
-        "id": "14BK3P6HigQOnNW6vAMc7RF5ioETKop4GUWxlZIPfmm0",
+        "id": _obtener_config("SHEET_ID_GENERAL"),
         "gid": "0",
         "ancla": "CC Prospecto",
     },
     "formacion": {
-        "id": "1J0u5EiA-82N1LTKT-Qt8qdJYI_sbc_hhjH33AWxCvZo",
+        "id": _obtener_config("SHEET_ID_FORMACION"),
         "gid": "225050212",
         "ancla": "Cedula",
     },
     "orientacion_consolidado": {
-        "id": "15pgRouShB-tUNbdtGYs-ziYeuDeqV0tl21Hb9lkDXto",
+        "id": _obtener_config("SHEET_ID_CONSOLIDADO_FCS"),
         "gid": "978755551",
         "ancla": "NÚMERO DE DOCUMENTO",
     },
     "remisiones": {
-        "id": "1EfxYCCEZsKqJK8Icou3hbZDBWEEm9T4VUK5EESI8ea0",
+        "id": _obtener_config("SHEET_ID_REMISIONES"),
         "gid": "0",
         "ancla": "NÚMERO DE DOCUMENTO",
     },
     "verificacion": {
-        "id": "1Ws-dR69INyipAU9bIVTL6VgCEJXkkfMpzm2H4Kaamm8",
+        "id": _obtener_config("SHEET_ID_VERIFICACION"),
         "gid": "1240996279",
         "ancla": "Número Documento",
     },
     "encuesta_basico_jco": {
-        "id": "1YeJUSrX8WnD_kf5Y8z5dsFb_mH7XNX9i8-EYP0aW-gA",
+        "id": _obtener_config("SHEET_ID_MITIGACION_ENCUESTA"),
         "gid": "851734049",
         "ancla": "CEDULA",
     },
     "encuesta_especializado": {
-        "id": "1YeJUSrX8WnD_kf5Y8z5dsFb_mH7XNX9i8-EYP0aW-gA",
+        "id": _obtener_config("SHEET_ID_MITIGACION_ENCUESTA"),
         "gid": "1685171350",
         "ancla": "CEDULA",
     },
     "parametros": {
-        "id": "14BK3P6HigQOnNW6vAMc7RF5ioETKop4GUWxlZIPfmm0",
+        "id": _obtener_config("SHEET_ID_GENERAL"),
         "gid": "1516261654",
         "ancla": "Mes",
     },
     "matriz_documental": {
-        "id": "1SLHtpzoL03kb0XWWzH1ZU_Ff4DhkNKuXlGHbjwyQcIQ",
+        "id": _obtener_config("SHEET_ID_MATRIZ_DOCUMENTAL"),
         "gid": "1167929471",
         "ancla": "CÉDULA",
     },
     "subsanaciones": {
-        "id": "15pgRouShB-tUNbdtGYs-ziYeuDeqV0tl21Hb9lkDXto",
+        "id": _obtener_config("SHEET_ID_CONSOLIDADO_FCS"),
         "gid": "1376346035",
         "ancla": "Documento",
     },
